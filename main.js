757f0f785977c9de0e196376d38c71265c23c312
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-12 16:27:37 +0100

    ajout index

---
+++ main.js
@@ -0,0 +1,37 @@
+// let myBody = document.getElementsByTagName("body")[0];
+
+// let element1 = myBody.firstElementChild;
+// console.log(element1);
+
+//////////////////
+
+// let element2 = myBody.lastElementChild;
+// console.log(element2);
+
+// let divElement = document.getElementsByTagName("div");
+
+////////////////
+
+// let div1 = divElement[0];
+// let exo4 = div1.children;
+
+// console.log(exo4);
+
+///////////////////
+
+//let liste = document.getElementsByTagName("li");
+
+// let listeElement1 = liste[0]
+// let listeElement2 = listeElement1.nextElementSibling
+// console.log(listeElement1);
+// console.log(listeElement2);
+
+/////
+
+//let element2 = liste[1];
+//let element1 = element2.previousElementSibling;
+//console.log(element1);
+
+
+
+
